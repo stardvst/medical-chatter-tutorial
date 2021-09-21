6900c28be1978f317b0f55c7f3fbc139ca51849b
Lilia <lilia.manukyan@hotmail.com>
2021-09-21 15:38:18 +0400

    setup twilio

--- server/index.js
+++ server/index.js
@@ -4,6 +4,11 @@ const authRoutes = require('./routes/auth');
 
 require('dotenv').config();
 
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const messagingServiceSid = process.env.TWILIO_MESSAGING_SERVICE_SID;
+const twilioClient = require('twilio')(accountSid, authToken);
+
 const PORT = process.env.PORT || 5000;
 
 const app = express();
@@ -15,6 +20,31 @@ app.use('/auth', authRoutes);
 
 app.get('/', (req, res) => {
   res.send('Hello world');
+});
+
+app.post('/', (req, res) => {
+  const { message, user: sender, type, members } = req.body;
+
+  if (type === 'message.new') {
+    members
+      .filter(member => member.user_id !== sender.id)
+      .forEach(({ user }) => {
+        if (!user.online) {
+          twilioClient.messages
+            .create({
+              body: `You have a new message from ${message.user.fullName} - ${message.text}`,
+              messagingServiceSid: messagingServiceSid,
+              to: user.phoneNumber,
+            })
+            .then(() => console.log('Message sent!'))
+            .catch(err => console.log(err));
+        }
+      });
+
+    return res.status(200).send('Message sent!');
+  }
+
+  return res.status(200).send('Not a new message request');
 });
 
 app.listen(PORT, () => {
